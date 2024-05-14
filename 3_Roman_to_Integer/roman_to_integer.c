1135bc9d5c3ed10e0ae50d9c9e2820d0b5958664
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2024-05-13 20:35:44 -0400

    Create roman_to_integer.c

---
+++ 3_Roman_to_Integer/roman_to_integer.c
@@ -0,0 +1,39 @@
+int romanToInt(char* s) {
+    int sum = 0;
+    char literal[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    int values[] = {1, 5, 10, 50, 100, 500, 1000};
+    int j1, j2;
+    int i = 0;
+
+    while (s[i] != '\0') {
+        for(int j = 0; j < sizeof(literal) / sizeof(literal[0]); j++){
+            if(s[i] == literal[j]){
+                j1 = j;
+                break;
+            }
+        }
+        
+        if (s[i + 1] != '\0') {
+            for(int j = 0; j < sizeof(literal) / sizeof(literal[0]); j++){
+                if(s[i] == literal[j]){
+                    j2 = j;
+                    break;
+                }
+            }
+            if(j1 < j2){
+                sum = sum + values[j2] - values[j1];
+                i = i + 2;
+            }
+            else{
+                sum = sum + values[j1] + values[j2];
+                i = i + 2;
+            } 
+        }
+        else {
+            sum = sum + values[j1];
+            i++;
+        }
+        
+    }
+    return sum;
+}
