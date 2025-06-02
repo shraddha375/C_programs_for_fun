0c10fdaf8a564834212354a382c71a8551c22261
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-06-02 19:58:30 -0400

    Update two_sum_python.py

--- 1_Two_Sum/two_sum_python.py
+++ 1_Two_Sum/two_sum_python.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_map = {}  # Stores number as key and its index as value
-        for i, num in enumerate(nums):
+        for i, num in enumerate(nums): # enumerate returns index and the number itself from the list
             complement = target - num
             if complement in num_map:
                 return [num_map[complement], i]
