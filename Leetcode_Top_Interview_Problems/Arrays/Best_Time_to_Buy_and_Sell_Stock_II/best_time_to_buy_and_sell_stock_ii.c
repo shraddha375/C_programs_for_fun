b73326cbce0a282bfd99a2b97d03acbeb1cc67b3
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-11-09 15:09:15 +0530

    Update best_time_to_buy_and_sell_stock_ii.c

--- Leetcode_Top_Interview_Problems/Arrays/Best_Time_to_Buy_and_Sell_Stock_II/best_time_to_buy_and_sell_stock_ii.c
+++ Leetcode_Top_Interview_Problems/Arrays/Best_Time_to_Buy_and_Sell_Stock_II/best_time_to_buy_and_sell_stock_ii.c
@@ -1,3 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
+
 int maxProfit(int* prices, int pricesSize) {
+
+    int profit = 0;
+
+    for(int i = 0; i < pricesSize - 1; i++) {
+        if (prices[i] < prices[i + 1]){
+            profit += (prices[i + 1] - prices[i]);
+        }
+    }
+
+    return profit;
     
 }
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};  // Input array
+    int expectedProfit = 7;    // Expected output
+    int pricesSize = sizeof(prices) / sizeof(prices[0]);
+    
+    int profit = maxProfit(prices, pricesSize);
+
+    // Judge checks
+    assert(profit == expectedProfit);
+
+    printf("Assertions passed.\n");
+    printf("Profit = %d\n", profit);
+    
+    return 0;
+}
+
