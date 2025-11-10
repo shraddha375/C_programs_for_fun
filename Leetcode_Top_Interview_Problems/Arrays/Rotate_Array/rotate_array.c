92fe46ce537d423448aaf11303e9153033e345e2
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-11-10 21:09:56 +0530

    Update rotate_array.c

--- Leetcode_Top_Interview_Problems/Arrays/Rotate_Array/rotate_array.c
+++ Leetcode_Top_Interview_Problems/Arrays/Rotate_Array/rotate_array.c
@@ -1,3 +1,40 @@
+#include <stdio.h>
+#include <assert.h>
+
 void rotate(int* nums, int numsSize, int k) {
-    
+    if (numsSize == 0) return;
+    k = k % numsSize;
+
+    int temp[numsSize];
+
+    for (int i = 0; i < numsSize; i++) {
+        temp[(i + k) % numsSize] = nums[i];
+    }
+
+    // Copy back into original array
+    for (int i = 0; i < numsSize; i++) {
+        nums[i] = temp[i];
+    }
 }
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5, 6, 7};  // Input array
+    int expectedNums[] = {5, 6, 7, 1, 2, 3, 4};    // Expected output
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int k = 3;
+
+    rotate(nums, numsSize, k);
+    
+    //for (int i = 0; i < numsSize; i++){
+    //    printf("nums = %d\n", nums[i]);
+    //}
+    
+    // Judge checks
+    for (int i = 0; i < numsSize; i++){
+        assert(nums[i] == expectedNums[i]);
+    }
+
+    printf("Assertions passed.\n");
+    return 0;
+}
+
