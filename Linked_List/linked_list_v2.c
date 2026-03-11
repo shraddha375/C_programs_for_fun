528685f2bda834e1a99be49e8a7676e46d10ee34
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2026-03-11 23:53:53 +0530

    Update linked_list_v2.c

--- Linked_List/linked_list_v2.c
+++ Linked_List/linked_list_v2.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{#include <stdio.h>
-#include <stdlib.h>
-
 struct Node{
     int value;
     struct Node* next;
@@ -125,58 +122,4 @@ struct Node* insertNode_position(struct Node* head, int value, int position){
     temp->next = newNode;
     
     return head;
-}
-    int value;
-    struct Node* next;
-};
-
-void addNode();
-void display(struct Node*);
-
-void freeList(struct Node*);
-
-int main(){
-    // allocate memory first because the pointer needs to point to valid memory
-    struct Node* first_node = malloc(sizeof(struct Node*));
-    first_node->value = 1;
-    first_node->next = NULL;
-    
-    struct Node* second_node = malloc(sizeof(struct Node*));
-    second_node->value = 2;
-    second_node->next = first_node;
-    
-    struct Node* third_node = malloc(sizeof(struct Node*));
-    third_node->value = 3;
-    third_node->next = second_node;
-    
-    struct Node* fourth_node = malloc(sizeof(struct Node*));
-    fourth_node->value = 4;
-    fourth_node->next = third_node;
-    
-    struct Node* head = fourth_node;
-    
-    display(head);
-    return 0;
-}
-
-void display(struct Node* head){
-    struct Node* iter = head;
-    
-    while(iter != NULL){
-        printf("Node value: %d\n", iter->value);
-        // No iter++ because these nodes are not consecutive memory locations
-        iter = iter->next;
-    }
-}
-
-void freeList(struct Node* head){
-    struct Node* iter = head;
-    struct Node* temp = iter->next;
-    
-    while(temp != NULL){
-        free(iter);
-        iter = temp;
-        temp = iter->next;
-        
-    }
 }
